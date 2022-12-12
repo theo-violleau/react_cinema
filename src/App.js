570de27f0d1a81e469015fdcf94f211ba1fa4d61
= <theo.violleau49@gmail.com>
2022-12-12 11:44:50 +0100

    no message

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import MovieList from './components/MovieList';
-import MovieListHeading from './components/MovieListHeading';
-import SearchBox from './components/SearchBox';
-import AddFavourites from './components/AddFavourites';
-import RemoveFavourites from './components/RemoveFavourites';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddFavourites from "./components/AddFavourites";
+import RemoveFavourites from "./components/RemoveFavourites";
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
 	const [favourites, setFavourites] = useState([]);
-	const [searchValue, setSearchValue] = useState('');
+	const [searchValue, setSearchValue] = useState("");
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=fc331929`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
@@ -29,7 +29,7 @@ const App = () => {
 
 	useEffect(() => {
 		const movieFavourites = JSON.parse(
-			localStorage.getItem('react-movie-app-favourites')
+			localStorage.getItem("react-movie-app-favourites")
 		);
 
 		if (movieFavourites) {
@@ -38,7 +38,7 @@ const App = () => {
 	}, []);
 
 	const saveToLocalStorage = (items) => {
-		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+		localStorage.setItem("react-movie-app-favourites", JSON.stringify(items));
 	};
 
 	const addFavouriteMovie = (movie) => {
@@ -57,22 +57,22 @@ const App = () => {
 	};
 
 	return (
-		<div className='container-fluid movie-app'>
-			<div className='row d-flex align-items-center mt-4 mb-4'>
-				<MovieListHeading heading='Movies' />
+		<div className="container-fluid movie-app">
+			<div className="row d-flex align-items-center mt-4 mb-4">
+				<MovieListHeading heading="Movies" />
 				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
 			</div>
-			<div className='row'>
+			<div className="row">
 				<MovieList
 					movies={movies}
 					handleFavouritesClick={addFavouriteMovie}
 					favouriteComponent={AddFavourites}
 				/>
 			</div>
-			<div className='row d-flex align-items-center mt-4 mb-4'>
-				<MovieListHeading heading='Favourites' />
+			<div className="row d-flex align-items-center mt-4 mb-4">
+				<MovieListHeading heading="Favourites" />
 			</div>
-			<div className='row'>
+			<div className="row">
 				<MovieList
 					movies={favourites}
 					handleFavouritesClick={removeFavouriteMovie}

96affa9349094a0b5e8c3c73ae0f1eb4aed2abcf
Erwan LE QUEAU <lequeauerwan@gmail.com>
2023-01-22 19:11:26 +0100

    add search bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ListFilm from "./components/ListFilm.js";
+import { Input, Empty } from "antd";
 
 function App() {
   const [film, setFilm] = useState([]);
   const [films, setFilms] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [titleSearched, setTitleSearched] = useState();
+
+  const apiUrl = "https://omdbapi.com/";
+  const apiKey = "fc331929";
 
   useEffect(() => {
     axios
-      .get("https://omdbapi.com/?t=Star%20Wars&apikey=fc331929")
+      .get("https://omdbapi.com/?t=Star%20Wars&apikey=" + apiKey)
       .then((res) => setFilm(res.data));
   }, []);
 
-  useEffect(() => {
+  function onSearch() {
+    setLoading(true);
     axios
-      .get("https://omdbapi.com/?s=Star%20Wars&apikey=fc331929")
-      .then((res) => setFilms(res.data.Search));
-  }, []);
+      .get(apiUrl + "?s=" + titleSearched + "&apikey=" + apiKey)
+      .then((res) => {
+        if (res.data.Search) {
+          setError(null);
+          setFilms(res.data.Search);
+          setLoading(false);
+        }
+        if (res.data.Error) {
+          setError(true);
+          setLoading(false);
+        }
+      });
+  }
 
-  console.log(films);
+  function getErrorView() {
+    return (
+      <Empty style={{ marginTop: 250 }} image={Empty.PRESENTED_IMAGE_SIMPLE} />
+    );
+  }
+
+  function getFilms() {
+    return <ListFilm films={films} />;
+  }
 
   return (
     <div>
-      <ListFilm films={films} />
+      <div style={{ justifyContent: "center", display: "flex" }}>
+        <Input.Search
+          placeholder="Cherchez un film"
+          allowClear
+          loading={loading}
+          onChange={(e) => setTitleSearched(e.target.value)}
+          onSearch={onSearch}
+          style={{
+            width: 250,
+            marginTop: 20,
+            marginBottom: 20,
+          }}
+        />
+      </div>
+      {error ? getErrorView() : getFilms()}
     </div>
   );
 }
